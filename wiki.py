8b133dbdb1998191c1b4a6b09f13d86802c29280
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-11-03 11:46:18 +0900

    XXX

--- wiki.py
+++ wiki.py
@@ -19,18 +19,33 @@ def get_matching_files():
 
 def get_files():
     return [f for f in glob.glob("{}**/*".format(wiki_dir))]
- 
+
+def show_help():
+    print
+    print("wiki -h              ... show help menu")
+    print("wiki -ls [kw1] [kw2] ... enlist pages with key words")
+    print("wiki -ra             ... randomly show articles")
+    print("wiki -e              ... add/edit wiki article")
+    print
+
 if(len(sys.argv) < 2):
-    title = raw_input("wiki title: ")
+    show_help()
 else:
     if  (sys.argv[1].startswith("-")):
         option = sys.argv[1]
         if  (option == "-h"):
-            print
-            print("wiki -h              ... show help menu")
-            print("wiki -ls [kw1] [kw2] ... enlist pages with key words")
-            print("wiki -ra             ... randomly show articles")
-            print
+            show_help()
+
+        elif(option == "-e"):
+            title = '_'.join(sys.argv[2:])
+
+            if(len(sys.argv) < 3):
+                title = raw_input("wiki title: ")
+            else:
+                title = title.replace(" ", "_")
+
+            os.system("vim {}/{}".format(wiki_dir, title))
+            print(title)
 
         elif(option == "-ra"):
             files = get_files()            
@@ -47,9 +62,3 @@ else:
 
         sys.exit()
 
-    title = '_'.join(sys.argv[1:])
-
-title = title.replace(" ", "_")
-os.system("vim {}/{}".format(wiki_dir, title))
-
-print(title)
